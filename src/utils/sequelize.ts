81660fbd688a03e3818b988f4eb2bd36652eec4d
Francesco Murador <francesco.murador@sistinf.it>
2024-09-09 16:26:53 +0200

    fix

--- src/utils/sequelize.ts
+++ src/utils/sequelize.ts
@@ -3,10 +3,17 @@ import { __DATABASE_SCHEMA__, __DATABASE_URL__ } from "../constants/environment"
 
 const sequelize = new Sequelize(__DATABASE_URL__, {
   dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // Questo può essere necessario per alcune configurazioni
+    },
+  },
   define: {
     schema: __DATABASE_SCHEMA__,
     timestamps: false,
   },
+  
   pool: {
     max: 5,
     min: 0,
