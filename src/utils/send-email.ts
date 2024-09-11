581d3c49f61acca5686f04a0c9e868db01091cb5
Francesco Murador <francesco.murador@sistinf.it>
2024-09-11 15:15:19 +0200

    abilito provider email

--- src/utils/send-email.ts
+++ src/utils/send-email.ts
@@ -1,20 +1,37 @@
 import nodemailer from "nodemailer";
-import { __ACCESS_TOKEN_, __CLIENT_ID__, __CLIENT_SECRET__, __EMAIL_USER__ } from "../constants/environment";
+import { google } from "googleapis";
+import {
+  __ACCESS_TOKEN_,
+  __CLIENT_ID__,
+  __CLIENT_SECRET__,
+  __EMAIL_USER__,
+} from "../constants/environment";
 
+const user = __EMAIL_USER__;
+const clientId = __CLIENT_ID__;
+const clientSecret = __CLIENT_SECRET__;
+
+const oauth2Client = new google.auth.OAuth2(
+  clientId,
+  clientSecret,
+  "https://developers.google.com/oauthplayground" // URL di reindirizzamento
+);
+
+// Ottieni l'access token
+const getAccessToken = async () => {
+  const { token } = await oauth2Client.getAccessToken();
+  return token;
+};
 const appUrl = "https://promoter-manager-server-0e30fdbde338.herokuapp.com"; // URL della tua applicazione (DA METTERE COME ENV)
 
 function createConfirmationLink(token: string) {
   return `${appUrl}/api/autenticazione/conferma-email/${token}`;
 }
 
-const accessToken = __ACCESS_TOKEN_;
-
-const user = __EMAIL_USER__;
-const clientId = __CLIENT_ID__;
-const clientSecret = __CLIENT_SECRET__;
 
 export async function sendConfirmationEmail(email: string, token: string) {
   const confirmationLink = createConfirmationLink(token);
+  const accessToken = await getAccessToken() as string;
   const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 465,
@@ -22,7 +39,7 @@ export async function sendConfirmationEmail(email: string, token: string) {
     auth: {
       user,
       accessToken,
-      type: 'OAuth2',
+      type: "OAuth2",
       clientId,
       clientSecret,
     },
