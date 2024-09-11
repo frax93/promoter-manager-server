48a66552dfc1b30886878a669b95ed2b60525adb
Francesco Murador <francesco.murador@sistinf.it>
2024-09-11 15:05:49 +0200

    abilito provider email

--- src/constants/environment.ts
+++ src/constants/environment.ts
@@ -14,3 +14,12 @@ export const __DATABASE_SCHEMA__ = process.env.DATABASE_SCHEMA || "promoter_mana
 
 export const __JWT_SECRET__ = process.env.JWT_SECRET || 'cd844fbaf51642dd4f14c498d8808fb540018f67f3acccf8455e864b3e0a2abe9987dde3180cfc0eb0becc9b64bd744001830de8c3427eae6c99e51731b88e3c';
 
+/** Client Email */
+export const __CLIENT_ID__ = process.env.CLIENT_ID || '206458219222-uaatakq3lkktt6cchrarj77d86farfd9.apps.googleusercontent.com';
+
+export const __ACCESS_TOKEN_ = process.env.ACCESS_TOKEN || '';
+
+export const __CLIENT_SECRET__ = process.env.CLIENT_SECRET || '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';
+
+export const __EMAIL_USER__ = process.env.EMAIL_USER || 'f41873773@gmail.com';
+
--- src/utils/send-email.ts
+++ src/utils/send-email.ts
@@ -1,4 +1,5 @@
 import nodemailer from "nodemailer";
+import { __ACCESS_TOKEN_, __CLIENT_ID__, __CLIENT_SECRET__, __EMAIL_USER__ } from "../constants/environment";
 
 const appUrl = "https://promoter-manager-server-0e30fdbde338.herokuapp.com"; // URL della tua applicazione (DA METTERE COME ENV)
 
@@ -6,18 +7,27 @@ function createConfirmationLink(token: string) {
   return `${appUrl}/api/autenticazione/conferma-email/${token}`;
 }
 
+
+const user = __EMAIL_USER__;
+const clientId = __CLIENT_ID__;
+const clientSecret = __CLIENT_SECRET__;
+
 export async function sendConfirmationEmail(email: string, token: string) {
   const confirmationLink = createConfirmationLink(token);
   const transporter = nodemailer.createTransport({
-    service: "gmail", // Puoi usare Gmail o un altro servizio
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
-      user: "f41873773@gmail.com",
-      pass: '6bJ)?(EWBgvCCv%Mdm:j',
+      user,
+      type: 'OAuth2',
+      clientId,
+      clientSecret,
     },
   });
 
   const mailOptions = {
-    from: "f41873773@gmail.com",
+    from: user,
     to: email,
     subject: "Conferma la tua registrazione",
     text: `Clicca sul seguente link per confermare la tua email: ${confirmationLink}`,
