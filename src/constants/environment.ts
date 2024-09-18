5b89902e5bc41a8b15beba1cdd1bef392d382aaf
Francesco Murador <francesco.murador@sistinf.it>
2024-09-18 10:28:28 +0200

    Update environment.ts

--- src/constants/environment.ts
+++ src/constants/environment.ts
@@ -12,14 +12,14 @@ export const __DATABASE_URL__ = process.env.DATABASE_URL || "postgres://postgres
 
 export const __DATABASE_SCHEMA__ = process.env.DATABASE_SCHEMA || "promoter_manager";
 
-export const __JWT_SECRET__ = process.env.JWT_SECRET || 'cd844fbaf51642dd4f14c498d8808fb540018f67f3acccf8455e864b3e0a2abe9987dde3180cfc0eb0becc9b64bd744001830de8c3427eae6c99e51731b88e3c';
+export const __JWT_SECRET__ = process.env.JWT_SECRET || '';
 
 /** Client Email */
-export const __CLIENT_ID__ = process.env.CLIENT_ID || '206458219222-uaatakq3lkktt6cchrarj77d86farfd9.apps.googleusercontent.com';
+export const __CLIENT_ID__ = process.env.CLIENT_ID || '';
 
-export const __REFRESH_TOKEN_ = process.env.REFRESH_TOKEN || '1//04dyPD-Q9EhL1CgYIARAAGAQSNwF-L9IrNYhf8XPXF5Y92sTIQjLGw_LTx2suippWlyaJ4jUQA1FjX37b58U65ypeWgze5NLZUwg';
+export const __REFRESH_TOKEN_ = process.env.REFRESH_TOKEN || '';
 
-export const __CLIENT_SECRET__ = process.env.CLIENT_SECRET || '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';
+export const __CLIENT_SECRET__ = process.env.CLIENT_SECRET || '';
 
-export const __EMAIL_USER__ = process.env.EMAIL_USER || 'f41873773@gmail.com';
+export const __EMAIL_USER__ = process.env.EMAIL_USER || '';
 
