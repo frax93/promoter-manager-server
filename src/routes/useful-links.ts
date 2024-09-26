b1c2d3beaec9ced69a84645d1b1d5707b7633251
Francesco Murador <francesco.murador@sistinf.it>
2024-09-26 11:27:26 +0200

    put link utili

--- src/routes/useful-links.ts
+++ src/routes/useful-links.ts
@@ -10,13 +10,39 @@ router.use(jwtMiddleware());
 router.get("/utente", async (req, res) => {
   const idUtente = req.user?.id;
   try {
-    const note = await LinkUtili.findAll({
+    const links = await LinkUtili.findAll({
       where: { utente_id: idUtente },
     });
-    res.json(note);
+    res.json(links);
   } catch (err) {
     console.error(err);
     res.status(500).send("Errore nel recupero dei link");
+  }
+});
+
+router.put("/", async (req, res) => {
+  const { links } = req.body; // links è un array di oggetti { id, url }
+  const userId = req.user?.id;
+
+  if (!Array.isArray(links)) {
+    return res.status(400).json({ message: "Richiesta non valida" });
+  }
+
+  try {
+    // Aggiorna i link esistenti
+    for (const link of links) {
+      if (link.id) {
+        await LinkUtili.update({ url: link.url }, { where: { id: link.id } });
+      } else {
+        // Crea un nuovo link
+        await LinkUtili.create({ url: link.url, userId });
+      }
+    }
+
+    return res.status(200).json({ message: "Link aggiornati con successo" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Errore del server" });
   }
 });
 
