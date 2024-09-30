a49fc513e25699c6dd3c42e0ee2b4e339b760751
Francesco Murador <francesco.murador@sistinf.it>
2024-09-30 11:29:28 +0200

    fix sui path email

--- src/db-models/user.ts
+++ src/db-models/user.ts
@@ -84,6 +84,15 @@ const Utente = sequelize.define(
           );
         }
       },
+      beforeSave: async (utente) => {
+        if (utente.dataValues.password) {
+          const salt = await bcrypt.genSalt(10);
+          utente.dataValues.password = await bcrypt.hash(
+            utente.dataValues.password,
+            salt
+          );
+        }
+      },
     },
   }
 );
